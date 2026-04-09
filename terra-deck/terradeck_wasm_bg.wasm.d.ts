8c51f0bb63f6375282471659471c2be8c5a8c961
Karim Lalani <jimmy00784@gmail.com>
2026-04-09 07:52:08 -0500

    Fix green bar behind player cards
    - Moved ATTACK button to render after player cards
    - Positioned button lower (y=520) to avoid overlap
    - Green button now appears cleanly below player cards

--- terra-deck/terradeck_wasm_bg.wasm.d.ts
+++ terra-deck/terradeck_wasm_bg.wasm.d.ts
@@ -2,13 +2,17 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const __wbg_canvasapplication_free: (a: number, b: number) => void;
+export const canvasapplication_execute_attack: (a: number) => void;
 export const canvasapplication_new: (a: number, b: number) => [number, number, number];
 export const canvasapplication_on_keydown: (a: number, b: any) => void;
 export const canvasapplication_on_mousedown: (a: number, b: any) => void;
-export const canvasapplication_on_mouseup: (a: number, b: any) => void;
 export const canvasapplication_render: (a: number) => void;
+export const canvasapplication_reset_game: (a: number) => void;
 export const canvasapplication_run: (a: number) => void;
+export const canvasapplication_update: (a: number, b: number) => void;
+export const canvasapplication_update_attack_button: (a: number) => void;
 export const canvasapplication_on_mousemove: (a: number, b: any) => void;
+export const canvasapplication_on_mouseup: (a: number, b: any) => void;
 export const __externref_table_alloc: () => number;
 export const __wbindgen_externrefs: WebAssembly.Table;
 export const __wbindgen_exn_store: (a: number) => void;
